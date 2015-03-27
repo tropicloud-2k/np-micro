7f4de7f4ab92944ab9f98856f97cb1611d6963e9
tropicloud <admin@tropicloud.net>
2015-03-26 21:51:35 -0300

    initial commit

--- nps/bin/nps-setup.sh
+++ nps/bin/nps-setup.sh
@@ -20,14 +20,6 @@ nps_setup() {
                 php-gd \
                 php-mcrypt
 	               
-	## WP-CLI
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
-	chmod +x /usr/local/bin/wp
-	
-	## JQ 
-	curl -O http://stedolan.github.io/jq/download/linux64/jq > /usr/local/bin/jq
-	chmod +x /usr/local/bin/jq
-	
 	# ------------------------
 	# CHROOT USER
 	# ------------------------
