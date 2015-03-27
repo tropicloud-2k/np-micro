eb6998a1ae5539b7cbba34305c8d3284b57cf9f0
tropicloud <admin@tropicloud.net>
2015-03-27 17:45:07 -0300

    Fri Mar 27 17:45:07 BRT 2015

--- np-cli/bin/np-setup.sh
+++ np-cli/bin/np-setup.sh
@@ -1,7 +1,5 @@
 np_setup() {
 
-	chmod +x /usr/local/np-cli/np && ln -s /usr/local/np-cli/np /usr/bin/np
-
 	# ------------------------
 	# INSTALL
 	# ------------------------
@@ -24,33 +22,32 @@ np_setup() {
 	rm -rf /var/cache/apk/*
                 
 	# ------------------------
-	# CHROOT USER
+	# CONFIG
 	# ------------------------
 		
+	chmod +x /usr/local/np-cli/np
+	ln -s /usr/local/np-cli/np /usr/bin/np
+	
 	adduser -G nginx -h $home -D $user
 	echo "source /etc/environment" >> $home/.bashrc
 	echo "$user    ALL=(ALL) ALL" >> /etc/sudoers
 	chown root:root $home && chmod 755 $home
 
-	mkdir -p $home/www
-	mkdir -p $home/ssl
+	mkdir -p $home/log
 	mkdir -p $home/run
+	mkdir -p $home/ssl
+	mkdir -p $home/www
 		
-	# ------------------------
-	# CONFIG
-	# ------------------------
-
-	cat $np/etc/nginx/default.conf > /etc/nginx/default.conf
-	cat $np/etc/php/php-fpm.conf > /etc/php/php-fpm.conf
-	cat $np/etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+	cp -R $np/etc/s6/* /app/run
 
 	cat $np/etc/html/index.html > $home/www/index.html
 	cat $np/etc/html/info.php > $home/www/info.php
-	
-	cp -R $np/etc/s6/* /app/run
+	cat $np/etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+	cat $np/etc/nginx/default.conf > /etc/nginx/default.conf
+	cat $np/etc/php/php-fpm.conf > /etc/php/php-fpm.conf
 	
 	# ------------------------
-	# SSL CERT.
+	# SSL
 	# ------------------------
 	
 	cd $home/ssl
@@ -59,11 +56,13 @@ np_setup() {
 	openssl req -nodes -sha256 -newkey rsa:2048 -keyout app.key -out app.csr -config openssl.conf -batch
 	openssl rsa -in app.key -out app.key
 	openssl x509 -req -days 365 -sha256 -in app.csr -signkey app.key -out app.crt
+	
 	rm -f openssl.conf
 	
 	# ------------------------
-	# FIX PERMISSIONS
+	# CHMOD
 	# ------------------------
 
-	chown $user:nginx -R $home/* && chmod 755 -R $home/*
+	chown $user:nginx -R $home/*
+	chmod 770 -R $home/*
 }
